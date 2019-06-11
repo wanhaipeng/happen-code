6d355d213c6732c91ec3171523bf065ce317b739
wanhaipeng <529057232@qq.com>
2019-06-11 23:12:02 +0800

    modify unique header

--- include/UniquePaths.hpp
+++ include/UniquePaths.hpp
@@ -13,25 +13,25 @@
 class Solution {
  private:
   std::vector<int> range(int value) {
-  	std::vector<int> data;
+    std::vector<int> data;
     for(int i = 0; i < value; i++) {
-      data.emplace_back(i);	
-	}
-	return std::move(data);
+      data.emplace_back(i);
+    }
+    return std::move(data);
   }
  public:
   Solution() = default;
   int uniquePaths(int m, int n) {
-	std::vector<std::vector<int>> paths(n, std::vector<int>(m, 0));
-	for (int row : range(n)) {
-	  for (int col : range(m)) {
-	    if (row == 0 || col == 0) {
-	      paths[row][col] = 1;
-		} else {
-		  paths[row][col] = paths[row - 1][col] + paths[row][col - 1];
-		}
-      }	
-	}
-	return paths[n - 1][m - 1];
+    std::vector<std::vector<int>> paths(n, std::vector<int>(m, 0));
+    for (int row : range(n)) {
+      for (int col : range(m)) {
+        if (row == 0 || col == 0) {
+          paths[row][col] = 1;
+        } else {
+          paths[row][col] = paths[row - 1][col] + paths[row][col - 1];
+        }
+      }
+    }
+    return paths[n - 1][m - 1];
   }
 };
